43bdf4664b07169b34c5294443246a2ee2224220
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-01-08 17:39:30 +0100

    refactored save method, added str and to_dict methods

--- models/base_model.py
+++ models/base_model.py
@@ -17,9 +17,15 @@ class BaseModel:
     Defines the base class for all the models
     in Benrays inventory management system.
     """
-    id: Mapped[str] =  mapped_column(String, primary_key=True)
-    created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False, default=datetime.utcnow)
-    updated_at: Mapped[datetime] = mapped_column(DateTime, nullable=False, default=datetime.utcnow)
+    id: Mapped[str] =  mapped_column(String(36), primary_key=True, sort_order=-3)
+    created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False,
+                                                    default=datetime.utcnow,
+                                                    sort_order=-2,
+                                                )
+    updated_at: Mapped[datetime] = mapped_column(DateTime, nullable=False,
+                                                    default=datetime.utcnow,
+                                                    sort_order=-1,
+                                                )
 
 
     def __init__(self, **kwargs):
@@ -37,19 +43,40 @@ class BaseModel:
         Updates timestamp for updated_at attribute and
         saves object to database.
         """
+        from models import storage
         self.updated_at = datetime.utcnow()
+        storage.new(self)
+        storage.save()
+
+    def __str__(self):
+        """ Returns a user friendly string representation of the object """
+        obj_dict = self.__dict__.copy()
+        if "_sa_instance_state" in obj_dict:
+            del obj_dict["_sa_instance_state"]
+        return f"[{self.__class__.__name__}] ({self.id}) {obj_dict}"
     
-    def __repr__(self):
-        """
-        Return an unambiguous string representation
-        of the object for debugging and logging.
-        """
-        last_item = list(self.__dict__.keys())[-1]
-        parameters = ""
-        for attr, value in self.__dict__.items():
-            if attr == last_item:
-                parameters = parameters + f"{attr}={value}"
-            else:
-                parameters = parameters + f"{attr}={value}" + ", "
-        return f"{self.__class__.__name__}({parameters})"
+    # def __repr__(self):
+    #     """
+    #     Return an unambiguous string representation
+    #     of the object for debugging and logging.
+    #     """
+    #     last_item = list(self.__dict__.keys())[-1]
+    #     parameters = ""
+    #     for attr, value in self.__dict__.items():
+    #         if attr == last_item:
+    #             parameters = parameters + f"{attr}={value}"
+    #         else:
+    #             parameters = parameters + f"{attr}={value}" + ", "
+    #     return f"{self.__class__.__name__}({parameters})"
+    
+    def to_dict(self):
+        """ Converts objects to dict representation """
+        obj_dict = {}
+        obj_dict.update(self.__dict__)
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+        obj_dict["__class__"] = self.__class__.__name__
+        if "_sa_instance_state" in obj_dict:
+            del obj_dict["_sa_instance_state"]
+        return obj_dict
 
