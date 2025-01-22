efd27a2857b8b39e0aaa48864d97c29e45b636c9
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-01-22 17:32:18 +0100

    added secret key, host and port env variables

--- web_flask/config.py
+++ web_flask/config.py
@@ -1,3 +1,14 @@
+#!/usr/bin/python3
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+secret_key = os.getenv("SECRET_KEY")
+host = os.getenv("BENRAYS_STORE_HOST")
+port = os.getenv("BENRAYS_STORE_PORT")
+
 BASE_URL = "http://127.0.0.1:5000/"
 BASE_PATH = "api/"
 VERSION = "v1"
