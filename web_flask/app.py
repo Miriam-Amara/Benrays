351c7d79aea9b2621a495597eb8f1369d01ee71a
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-01-22 17:31:08 +0100

    added secret key, modified host and port

--- web_flask/app.py
+++ web_flask/app.py
@@ -3,12 +3,24 @@
 
 """
 from benrays_store import app
+from config import secret_key, host, port
+
+app.config['SECRET_KEY'] = secret_key
 
 for rule in app.url_map.iter_rules():
     print(rule)
+print("Search paths for templates:", app.jinja_loader.searchpath)
+print(app.jinja_env.get_or_select_template("base.html"))
+print(app.jinja_env.get_or_select_template("employee.html"))
+
+
 
 
 
 
 if __name__ == "__main__":
-    app.run(threaded=True, debug=True, host="0.0.0.0", port=5000)
+    if not host:
+        host = "0.0.0.0"
+    if not port:
+        port = 5000
+    app.run(threaded=True, debug=True, host=host, port=port)
