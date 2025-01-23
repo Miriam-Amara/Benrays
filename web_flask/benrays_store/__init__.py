1e8acde90942270dcee810748e80ca36684c6ec0
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-01-23 13:16:34 +0100

    added bcrypt

--- web_flask/benrays_store/__init__.py
+++ web_flask/benrays_store/__init__.py
@@ -1,7 +1,10 @@
 from benrays_store.users.employees.employees_routes import employee_bp
+
 from flask import Flask
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+bycrpt = Bcrypt(app)
 
 app.register_blueprint(employee_bp, url_prefix="/benrays_store/employees")
 
