da725d2b82bddc8e816402b9cb8579432331f82c
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-01-23 13:17:39 +0100

    added headers, modified employees function

--- web_flask/config.py
+++ web_flask/config.py
@@ -9,11 +9,11 @@ secret_key = os.getenv("SECRET_KEY")
 host = os.getenv("BENRAYS_STORE_HOST")
 port = os.getenv("BENRAYS_STORE_PORT")
 
-BASE_URL = "http://127.0.0.1:5000/"
+BASE_URL = "http://127.0.0.1:4000/"
 BASE_PATH = "api/"
 VERSION = "v1"
-
 EMPLOYEES = "/employees"
+headers = {"Content-Type": "application/json"}
 
-def get_employees():
-    return BASE_URL + BASE_PATH + VERSION + EMPLOYEES
+def employees(PATH=""):
+    return BASE_URL + BASE_PATH + VERSION + EMPLOYEES + PATH
