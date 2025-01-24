fafaa77782656c85e969ea7af18cb568cd2c49f7
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-01-24 16:34:46 +0100

    added update route

---
+++ api/v1/views/employees/update.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+"""
+This module contains UpdateEmployee class for
+updating the information of an employee in
+Benrays Store database.
+"""
+
+from models import storage
+from models.employees import Employee
+from views.employees import employees_bp
+from views.employees.utils import check_required_attributes
+from views.employees.utils import delete_attr
+
+from flask_restful import Api, Resource, abort
+
+
+api = Api(employees_bp)
+
+class UpdateEmployee(Resource):
+    """
+    A class to update an employee information
+    in the database.
+    """
+
+    def put(self, id):
+        """
+        Checks if employee exists in the database
+        and updates the info.
+        """
+        data = check_required_attributes(passwd=False)
+        if data["warehouses"]:
+            del data["warehouses"]
+        del data["password"]
+
+        employee_exists = storage.get(Employee, id=id)
+        if not employee_exists:
+            abort(404, description="Employee not found")
+
+        employee = list(employee_exists.values())[0]
+        employee_data = employee.to_dict()
+
+        for attr, value in data.items():
+            if value != employee_data.get(attr):
+                # print("value from database: ", employee_data.get(attr), type(employee_data.get(attr)))
+                setattr(employee, attr, value)
+        employee.save()
+
+        delete_data = ["password", "permissions", "__class__"]
+        delete_attr(employee_dict=employee_data, *delete_data)
+
+        return employee_data, 200
+    
+api.add_resource(UpdateEmployee, "/update/<id>")
