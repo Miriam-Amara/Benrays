c8d377c63969db7ded63dda7f9df763e216392d4
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-01-26 21:51:09 +0100

    Registered login blueprint. Initialized login_manager

--- web_flask/benrays_store/__init__.py
+++ web_flask/benrays_store/__init__.py
@@ -1,10 +1,14 @@
 from benrays_store.users.employees.employees_routes import employee_bp
+from benrays_store.users.employees.auth_manager import login_manager
+from benrays_store.login import login_bp
 
 from flask import Flask
 from flask_bcrypt import Bcrypt
 
+
 app = Flask(__name__)
 bycrpt = Bcrypt(app)
+login_manager.init_app(app)
 
 app.register_blueprint(employee_bp, url_prefix="/benrays_store/employees")
-
+app.register_blueprint(login_bp, url_prefix="/benrays_store/users")
