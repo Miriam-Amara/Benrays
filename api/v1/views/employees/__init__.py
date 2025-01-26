d23bc7818397edfdebf91d41ccaec26f54c46584
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-01-26 21:33:49 +0100

    imported delete module

--- api/v1/views/employees/__init__.py
+++ api/v1/views/employees/__init__.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 
 employees_bp = Blueprint("employees", __name__)
 
-from views.employees.create import *
+from api.v1.views.employees.create import *
 from api.v1.views.employees.get_employee import *
 from api.v1.views.employees.update import *
 from api.v1.views.employees.delete import *
