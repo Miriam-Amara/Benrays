42d0769fde45423412debaee12e00685d4e20afc
Miriam-Amara <ikechukwumiriam@gmail.com>
2025-01-26 21:32:27 +0100

    registered products blueprint

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,4 +1,5 @@
-from views.employees import employees_bp
+from api.v1.views.employees import employees_bp
+from api.v1.views.products import products_bp
 
 from flask import Flask
 from flask_bcrypt import Bcrypt
@@ -7,4 +8,4 @@ app =Flask(__name__)
 bcrypt = Bcrypt(app)
 
 app.register_blueprint(employees_bp, url_prefix="/api/v1/employees")
-
+app.register_blueprint(products_bp, url_prefix="/api/v1/products")
